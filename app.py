c513da91fc55f2773a3acf3f634464fb3c927535
Charanpreet Singh <charanpreetsingh627@gmail.com>
2026-05-01 00:16:02 +0530

    Add static file serving and home route

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
 from routes.chat import router as chat_router
 
 app = FastAPI()
 
 app.include_router(chat_router)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html"
+    )
